667804a5c18cabe156914680ddabffcfdef0c71a
Thorsten Wilmer <wilmer.thorsten@gmail.com>
2015-10-25 09:08:05 +0530

    Configure pin for UART0

---
+++ eagle/zip.sh
@@ -0,0 +1,3 @@
+a=$1
+rm $a.zip
+zip $a.zip $a.G* $a.TXT $a.brd $a.csv
--- workspace/targetboard/src/main.c
+++ workspace/targetboard/src/main.c
@@ -11,7 +11,7 @@
 void SysTick_Handler(void)
 {
 
-	Board_LED_Toggle(3);
+	//Board_LED_Toggle(3);
 	//Board_LED_Toggle(1);
 	//Board_LED_Toggle(2);
 }
@@ -73,6 +73,117 @@ void USB_IRQHandler(void)
 	USBD_API->hw->ISR(g_hUsb);
 }
 
+uint32_t values[500]={0};
+uint32_t values_low[500]={0};
+uint32_t values_high[500]={0};
+uint32_t valuepos=0;
+uint32_t valuelowpos=0;
+uint32_t valuehighpos=0;
+uint32_t lowMin=0xFFFFFFFF;
+uint32_t lowMax=0;
+uint32_t highMin=0xFFFFFFFF;
+uint32_t highMax=0;
+uint32_t start=0;
+uint32_t received=0;
+uint32_t receivedNow=0;
+uint32_t receivedValues[32];
+uint32_t receivedValuesPos=0;
+uint32_t bitsrecevied[500]={0};
+uint32_t bitsreceviedPos=0;
+uint32_t bitCount;
+void EINT3_IRQHandler(void)
+{
+	uint32_t curValue=0;
+
+	if(Chip_GPIOINT_IsIntPending(LPC_GPIOINT, 0))
+	{
+
+		int falling=(Chip_GPIOINT_GetStatusFalling(LPC_GPIOINT,0) & (1<<24));
+		int rising=(Chip_GPIOINT_GetStatusRising(LPC_GPIOINT,0) & (1<<24));
+		if(  falling &&
+				rising
+		)
+		{
+
+		}else {
+			curValue=Chip_TIMER_ReadCount(LPC_TIMER0);
+			Chip_TIMER_Reset(LPC_TIMER0);
+			Chip_TIMER_Enable(LPC_TIMER0);
+			if(falling)
+			{
+
+				Chip_GPIOINT_SetIntRising(LPC_GPIOINT,GPIOINT_PORT0,1<<24);
+
+				if(start==1)
+				{
+					received <<=1;
+					bitCount++;
+					if(curValue > 14722)
+					{
+						received|=1;
+					}
+					values_low[valuelowpos]=curValue;
+					valuelowpos++;
+					if(valuelowpos>=500)
+						valuelowpos=499;
+				}
+
+			}else if(rising)
+			{
+				if(start==1)
+				{
+					received <<=1;
+					bitCount++;
+					if(curValue > 14722)
+					{
+						received|=1;
+					}
+					values_low[valuelowpos]=curValue;
+					valuelowpos++;
+					if(valuelowpos>=500)
+						valuelowpos=499;
+				}
+
+				// LG remoteif(curValue>208468 && curValue<900000)
+				if(curValue>60433 && curValue<900000)
+				{
+					received=0;
+					start=1;
+					bitCount=0;
+
+			    // 	Board_LED_Set(1, 1);
+				}
+
+
+
+			}
+			if(  bitCount==33 || bitCount==35)
+			{
+				receivedNow=received;
+				receivedValues[receivedValuesPos]=received;
+				if(receivedValuesPos<31)
+					receivedValuesPos++;
+
+			}
+
+			if(start==1)
+			{
+				values[valuepos]=curValue;
+								if(valuepos<499)
+									valuepos++;
+
+			}
+		}
+
+
+		Chip_GPIOINT_ClearIntStatus(LPC_GPIOINT, 0,1<<24);
+
+
+	}
+	NVIC_ClearPendingIRQ(EINT3_IRQn);
+
+}
+
 /* Find the address of interface descriptor for given class type. */
 USB_INTERFACE_DESCRIPTOR *find_IntfDesc(const uint8_t *pDesc, uint32_t intfClass, int occ)
 {
@@ -123,6 +234,12 @@ int main(void)
 	/* enable clocks and pinmux */
 	usb_pin_clk_init();
 
+
+
+
+	DEBUGSTR("Test");
+	while(1);
+
 	/* initialize call back structures */
 	memset((void *) &usb_param, 0, sizeof(USBD_API_INIT_PARAM_T));
 	usb_param.usb_reg_base = LPC_USB_BASE + 0x200;
@@ -148,6 +265,25 @@ int main(void)
 	Chip_GPIO_SetPinDIROutput(LPC_GPIO,1,18);
 
 
+
+	// Stuff for IR, enable Pin I/O Interrupt
+	Chip_GPIOINT_Init(LPC_GPIOINT);
+	NVIC_SetPriority(EINT3_IRQn, 1);
+	Chip_GPIOINT_SetIntFalling(LPC_GPIOINT,GPIOINT_PORT0,1<<24);
+	//Chip_GPIOINT_SetIntRising(LPC_GPIOINT,GPIOINT_PORT0,1<<24);
+	Chip_GPIOINT_ClearIntStatus(LPC_GPIOINT, 0,1<<24);
+	NVIC_ClearPendingIRQ(EINT3_IRQn);
+	 NVIC_EnableIRQ(EINT3_IRQn);
+	 Chip_TIMER_Init(LPC_TIMER0);
+
+	 for(int i=0;i<500;i++)
+	 {
+	 values[i]=0;
+	  values_low[i]=0;
+	 values_high[i]=0;
+	 }
+	 start=0;
+
 	/* USB Initialization */
 	ret = USBD_API->hw->Init(&g_hUsb, &desc, &usb_param);
 	if (ret == LPC_OK) {
@@ -169,13 +305,42 @@ int main(void)
 	Board_UARTPutSTR("ABC\n");
 	uint8_t g_rxBuff[256];
 	uint32_t prompt = 0, rdCnt = 0;
+
+
+	//ESP into reset
+	Chip_GPIO_SetPinState(	LPC_GPIO, 0 ,6 ,0);
+	__enable_irq();
+	int count=0;
+	bitsreceviedPos=0;
 	while (1) {
-		int r =Buttons_GetStatus();
-		if(r==BUTTONS_BUTTON2)
+
+		switch(receivedNow)
 		{
+		case 0x418030E7:
+		case 0x4300C39D:
+			count=1000000;
+			Board_LED_Set(0, 1);
+			break;
+		case 1124123539:
+		case 100713363:
 			Board_LED_Set(1, 1);
-		}else{
-			Board_LED_Set(1, 0);
+			count=1000000;
+			break;
+		case 1124123545:
+		case 100713369:
+			Board_LED_Set(2, 1);
+			count=1000000;
+			break;
 		}
+		receivedNow=0;
+		if(count>0)
+		{
+			count--;
+		}else{
+			Board_LED_Set(0, 0);
+			Board_LED_Set(1, 0);
+			Board_LED_Set(2, 0);
+		}
+
 	}
 }
